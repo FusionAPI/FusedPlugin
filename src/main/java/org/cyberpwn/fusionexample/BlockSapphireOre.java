2e04f567ecc98e2eaa7b2ab02b11cd98295cb1f2
Dan <danielmillst@gmail.com>
2017-07-22 01:28:43 -0400

    Update api use

---
+++ src/main/java/org/cyberpwn/fusionexample/BlockSapphireOre.java
@@ -0,0 +1,11 @@
+package org.cyberpwn.fusionexample;
+
+import org.cyberpwn.fusionapi.type.FusionMaterial;
+
+public class BlockSapphireOre extends FusionMaterial
+{
+	public BlockSapphireOre()
+	{
+		super("texture_sapphire_ore");
+	}
+}
--- src/main/java/org/cyberpwn/fusionexample/ExamplePlugin.java
+++ src/main/java/org/cyberpwn/fusionexample/ExamplePlugin.java
@@ -2,10 +2,16 @@ package org.cyberpwn.fusionexample;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.cyberpwn.fusionapi.registry.Fuse;
-import org.cyberpwn.fusionapi.registry.FuseMod;
+import org.cyberpwn.fusionapi.registry.Registrant;
+import org.cyberpwn.fusionapi.registry.Registrar;
+import org.cyberpwn.fusionapi.type.FMaterial;
+import org.cyberpwn.fusionapi.type.FModel;
+import org.cyberpwn.fusionapi.type.FShader;
+import org.cyberpwn.fusionapi.type.FSound;
+import org.cyberpwn.fusionapi.type.FTexture;
+import org.cyberpwn.fusionapi.type.FusionTexture;
 
 // Implement Fuse to notify we are a fuse plugin
-@FuseMod(source = "exampleplugin")
 public class ExamplePlugin extends JavaPlugin implements Fuse
 {
 	@Override
@@ -20,8 +26,20 @@ public class ExamplePlugin extends JavaPlugin implements Fuse
 		
 	}
 	
-	public void onRegister()
+	public String getSource()
 	{
-		// Register our data
+		return getName().toLowerCase();
+	}
+	
+	public void onRegister(Registrar registrar)
+	{
+		Registrant<FMaterial> materialReg = registrar.getMaterialRegistrant();
+		Registrant<FSound> soundReg = registrar.getSoundRegistrant();
+		Registrant<FTexture> textureReg = registrar.getTextureRegistrant();
+		Registrant<FModel> modelReg = registrar.getModelRegistrant();
+		Registrant<FShader> shaderReg = registrar.getShaderRegistrant();
+		
+		textureReg.register("texture_sapphire_ore", new FusionTexture());
+		materialReg.register("material_sapphire_ore", new BlockSapphireOre());
 	}
 }
